c67f0bdd24d676654bf351aa1126a9de35a3840f
dr.elhabib <work.elhabib@gmail.com>
2023-01-22 23:10:13 +0100

    fix

--- main.py
+++ main.py
@@ -6,57 +6,28 @@ import numpy as np
 import cv2
 
 
-def sdvt_im_org(img):
-    w, h = img.shape[:2]
-    image = []
-    # image_dup = []
-    for i in range(w):
-        for j in range(h):
-            x = img[i][j]
-            image_col = []
-            image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                    1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,0,0)
-            image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                    1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,0,0)
-            image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                    1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,255,0)
-            image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                    1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,0,255)
-            image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                    1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,255,0)
-            image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                    1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,0,255)
-            image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                    1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,255,255)
-            image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                    1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,255,255)
-            image_col.append(np.std([image_col[k] for k in range(8)]))  # ecart type des distances
-            if not any((image_col == x) for x in image):
-                image.append(image_col)
-    return image
-
-
 def sdvt_im_org2(data):
     image = []
     for x in data:
         image_col = []
-        image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,0,0)
-        image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,0,0)
-        image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,255,0)
-        image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,0,255)
-        image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                1 + x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,255,0)
-        image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 + x[1]) ** 2 + (
-                1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,0,255)
-        image_col.append(np.sqrt((1 + x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,255,255)
-        image_col.append(np.sqrt((1 - x[0]) ** 2 + (1 - x[1]) ** 2 + (
-                1 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,255,255)
+        image_col.append(np.sqrt((x[0]) ** 2 + (x[1]) ** 2 + (
+            x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,0,0)
+        image_col.append(np.sqrt((255 - x[0]) ** 2 + (x[1]) ** 2 + (
+            x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,0,0)
+        image_col.append(np.sqrt((x[0]) ** 2 + (255 - x[1]) ** 2 + (
+            x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,255,0)
+        image_col.append(np.sqrt((x[0]) ** 2 + (x[1]) ** 2 + (
+                255 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,0,255)
+        image_col.append(np.sqrt((255 - x[0]) ** 2 + (255 - x[1]) ** 2 + (
+            x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,255,0)
+        image_col.append(np.sqrt((255 - x[0]) ** 2 + (x[1]) ** 2 + (
+                255 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,0,255)
+        image_col.append(np.sqrt((x[0]) ** 2 + (255 - x[1]) ** 2 + (
+                255 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (0,255,255)
+        image_col.append(np.sqrt((255 - x[0]) ** 2 + (255 - x[1]) ** 2 + (
+                255 - x[2]) ** 2))  # distance entre pixel et sommet de coordonnee (255,255,255)
         image_col.append(np.std([image_col[k] for k in range(8)]))  # ecart type des distances
+        image_col.append(x)
         image.append(image_col)
     return image
 
@@ -65,7 +36,6 @@ def sdvt_im_org2(data):
 def steps(data, min_size):
     while len(data) > min_size:
         data = step(data)
-        print(len(data))
     return data
 
 
@@ -79,21 +49,22 @@ def step(data):
         if len(data) == 1:
             new_data.append(
                 [data[0][0], data[0][1], data[0][2], data[0][3], data[0][4], data[0][5], data[0][6], data[0][7],
-                 data[0][8]])
+                 data[0][8],data[0][9]])
             break
         else:
             for other_key in range(1, len(data)):
-                diff = abs(data[0][8] - data[other_key][8])
-                if diff < min_diff:
-                    min_diff = diff
-                    index_min = other_key
+                if not all(item in data[0][:8] for item in data[other_key][:8]):
+                    diff = abs(data[0][8] - data[other_key][8])
+                    if diff < min_diff:
+                        min_diff = diff
+                        index_min = other_key
 
             if index_min != -1:
                 new_data.append([(data[0][0] + data[index_min][0]) / 2, (data[0][1] + data[index_min][1]) / 2,
                                  (data[0][2] + data[index_min][2]) / 2, (data[0][3] + data[index_min][3]) / 2,
                                  (data[0][4] + data[index_min][4]) / 2, (data[0][5] + data[index_min][5]) / 2,
                                  (data[0][6] + data[index_min][6]) / 2, (data[0][7] + data[index_min][7]) / 2,
-                                 (data[0][8] + data[index_min][8]) / 2])
+                                 (data[0][8] + data[index_min][8]) / 2,(data[0][9] + data[index_min][9]) / 2])
             else:
                 print("/////////////////")
             del data[index_min]
@@ -120,7 +91,21 @@ def no_deps(img):
 
 
 start_time = time.time()
-img = cv2.imread('image.png')
+# img = cv2.imread('image.png')
+# img = cv2.imread('image_edit_2.png')
+# img = cv2.imread('image_edit_2.png')
+# array_data = no_deps(img)
+
+# array_data = sdvt_im_org2(array_data)
+# print(steps(array_data, 8))
+img = cv2.imread('image_edit_1.png')
+# img = cv2.imread('image_edit_2.png')
+# img = cv2.imread('image_edit_2.png')
+array_data = no_deps(img)
+
+array_data = sdvt_im_org2(array_data)
+print(steps(array_data, 8))
+img = cv2.imread('image_edit_2.png')
 # img = cv2.imread('image_edit_2.png')
 # img = cv2.imread('image_edit_2.png')
 array_data = no_deps(img)
